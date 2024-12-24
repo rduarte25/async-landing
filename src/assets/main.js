446f468a529c99e661cef38d9aa53aac95ef6fa5
Rafael Duarte <elyouus94@gmail.com>
2024-12-24 16:00:28 -0400

    Finish

---
+++ src/assets/main.js
@@ -0,0 +1,46 @@
+const API =
+  "https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=9";
+const content = null || document.getElementById("content");
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "d9e4d74f13msh100e428c0553e6dp1a14aajsnb9406deb7241",
+    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+  },
+};
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items
+      .map(
+        (video) => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+            >
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+    `
+      )
+      .slice(0, 4)
+      .join("")}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
